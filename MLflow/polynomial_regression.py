5fd46ba7e21bcb321a0d19eecd4049b1c3b02f8a
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2022-12-21 12:10:34 +0530

    polynomial regression code

---
+++ MLflow/polynomial_regression.py
@@ -0,0 +1,29 @@
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy
+import matplotlib.pyplot as plt
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 3))
+
+myline = numpy.linspace(0, 6, 100)
+
+plt.scatter(train_x, train_y)
+plt.plot(myline, mymodel(myline))
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
